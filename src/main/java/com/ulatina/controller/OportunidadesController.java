e7734d60328ca7ae74cc927a127e7d5601841b22
Jaime Fung Delgado <jaimefung1159@gmail.com>
2025-03-17 20:13:04 -0600

    Update OportunidadesController.java

--- src/main/java/com/ulatina/controller/OportunidadesController.java
+++ src/main/java/com/ulatina/controller/OportunidadesController.java
@@ -5,6 +5,7 @@
 package com.ulatina.controller;
 
 import com.ulatina.data.Oportunidades;
+import com.ulatina.data.Organizacion;
 import com.ulatina.data.Postulaciones;
 import com.ulatina.service.Servicio;
 import com.ulatina.service.ServicioOportunidad;
@@ -23,18 +24,36 @@ import java.util.List;
  *
  * @author Ryzon
  */
-
-
 @Named
 @SessionScoped
 
-public class OportunidadesController implements Serializable{
-    
+public class OportunidadesController implements Serializable {
+
     private Oportunidades oportunidades = new Oportunidades();
     private ServicioOportunidad servicioOportunidad = new ServicioOportunidad();
     private List<Oportunidades> listaOportunidades = new ArrayList<>();
-        private ServicioPostulaciones servicioPostulaciones = new ServicioPostulaciones();
-        private Postulaciones postulaciones = new Postulaciones();
+    private ServicioPostulaciones servicioPostulaciones = new ServicioPostulaciones();
+    private Postulaciones postulaciones = new Postulaciones();
+    private String filtroBusqueda;
+
+    @PostConstruct
+    public void init() {
+
+        cargarOportunidades();
+
+    }
+
+    Servicio servicio = new Servicio() {
+
+    };
+
+    public String getFiltroBusqueda() {
+        return filtroBusqueda;
+    }
+
+    public void setFiltroBusqueda(String filtroBusqueda) {
+        this.filtroBusqueda = filtroBusqueda;
+    }
 
     public Postulaciones getPostulaciones() {
         return postulaciones;
@@ -44,22 +63,6 @@ public class OportunidadesController implements Serializable{
         this.postulaciones = postulaciones;
     }
 
-
-    @PostConstruct
-    public void init() throws ClassNotFoundException {
-        
-        cargarOportunidades();
-        
-    }
-       public String irADetalles(Oportunidades op) {
-        this.oportunidades = op;
-       
-        return "verDetallesOportunidad?faces-redirect=true";
-    }
-    Servicio servicio = new Servicio(){
-        
-    };
-
     public ServicioOportunidad getServicioOportunidad() {
         return servicioOportunidad;
     }
@@ -67,8 +70,6 @@ public class OportunidadesController implements Serializable{
     public void setServicioOportunidad(ServicioOportunidad servicioOportunidad) {
         this.servicioOportunidad = servicioOportunidad;
     }
-
-   
 
     public Oportunidades getOportunidades() {
         return oportunidades;
@@ -94,51 +95,53 @@ public class OportunidadesController implements Serializable{
         this.servicio = servicio;
     }
 
-    
+    public String irADetalles(Oportunidades op) {
+        this.oportunidades = op;
 
-    
+        return "verDetallesOportunidad?faces-redirect=true";
+    }
 
- public void aplicar(Postulaciones postulaciones, int idOportunidad, int idUsuario) throws SQLException {
+    public void aplicar(int idOportunidad, int idUsuario) throws SQLException {
         try {
             servicioPostulaciones.insertarPostulacion(postulaciones, idOportunidad, idUsuario);
-            
+
             FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Oportunidad creada", "Oportuinidad creada con exito"));
-            
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Oportunidad creada", "Oportuinidad creada con exito"));
+
             servicio.redireccionar("/pasantias.xhtml");
-            
+
         } catch (ClassNotFoundException e) {
             FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error inesperado", "No se pudo completar la oportunidad:  " + e.getMessage()));
-        }
-    }
-   
-    
-    
-    public void registrarOportunidad() {
-        try {
-            servicioOportunidad.insertarOportunidad(oportunidades);
-            FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Oportunidad creada", "Oportuinidad creada con exito"));
-            
-            servicio.redireccionar("/landingPageOrganizacion.xhtml");
-            
-        } catch (Exception e) {
-            FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error inesperado", "No se pudo completar la oportunidad:  " + e.getMessage()));
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error inesperado", "No se pudo completar la oportunidad:  " + e.getMessage()));
         }
     }
 
-    public void cargarOportunidades() throws ClassNotFoundException {
+    public void registrarOportunidad(String correo) {
+        try {
+            servicioOportunidad.insertarOportunidad(oportunidades, correo);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Oportunidad creada", "Oportuinidad creada con exito"));
+
+            servicio.redireccionar("/landingPageOrganizacion.xhtml");
+
+        } catch (ClassNotFoundException e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error inesperado", "No se pudo completar la oportunidad:  " + e.getMessage()));
+        }
+    }
+
+    public void cargarOportunidades() {
 
         this.listaOportunidades = servicioOportunidad.cargarOportunidades();
 
     }
-    
-    
+
+    public void filtrarOportunidades() {
+        servicioOportunidad.setFiltro(filtroBusqueda);
+        this.listaOportunidades = servicioOportunidad.cargarOportunidades();
+    }
+
     public String onFlowProcess(org.primefaces.event.FlowEvent event) {
         return event.getNewStep();
     }
 }
-                                                                                                                                                                                                
-                                                                                                                                                                                               
